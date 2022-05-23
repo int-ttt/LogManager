c9adb29d203785b92a0572b2a61f2186ba6a1daa
int_t <63438790+peanut-exe@users.noreply.github.com>
2022-05-23 19:54:13 +0900

    Initial commit

---
+++ src/Main/java/net/intt/util/LogManager.java
@@ -0,0 +1,29 @@
+package net.intt.util;
+
+import java.io.File;
+
+public class LogManager {
+
+    final String projectName;
+
+    public LogManager(String projectName){
+        this.projectName = projectName;
+    }
+
+    public void warn(Object value) {
+        var s = "\u001B[31m[WARN] " + projectName + ": " + value + "\u001B[0m";
+        System.out.println(s);
+    }
+    public void info(Object value) {
+        var s = "\u001B[33m[INFO] " + projectName + ": " + value + "\u001B[0m";
+        System.out.println(s);
+    }
+    public void error(Object value) {
+        var s = "\u001B[91m[ERROR] " + projectName + ": " + value + "\u001B[0m";
+        System.out.println(s);
+    }
+    public void debug(Object value) {
+        var s = "\u001B[32m[DEBUG] " + projectName + ": " + value + "\u001B[0m";
+        System.out.println(s);
+    }
+}
